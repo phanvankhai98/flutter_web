d77c309110316fcada0abe2d8dfb0e314515effc
KhaiPV <pvkhaicd1998@gmail.com>
2022-02-21 23:13:51 +0700

    first commit, Hope i can finish it :((

---
+++ wongroup/lib/src/pages/home/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get_boilerplate/src/public/constant.dart';
+import 'package:get_boilerplate/src/theme/theme_service.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.refresh,
+          size: width / 16.0,
+        ),
+        onPressed: () {
+          ThemeService().changeThemeMode();
+        },
+      ),
+      body: Column(
+        children: [
+          Row()
+        ],
+      ),
+    );
+  }
+}
